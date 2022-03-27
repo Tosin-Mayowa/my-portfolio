239fc342383e83578da4d829105bfdabcab57df1
Tosin Mayowa <toss800@gmail.com>
2022-03-27 18:51:25 +0100

    fix about me

--- src/Component/About/About.js
+++ src/Component/About/About.js
@@ -1,26 +1,29 @@
 import React from 'react'
-
+import '../../../node_modules/bootstrap/dist/css/bootstrap.min.css';
 
 import './About.css';
 const About = () => {
   return (
-    <div className='About'>
-    <div className='About-left'>
-    <h1 className='About-Hl'><small>Meet</small> <br/> Mayowa Tosin Adejumola</h1>
+    <div className='container-fluid About mt-0'>
+    <div className='row'>
+    <div className='col-xs-12 col-md-12 col-lg-4 About-Hl'>
+    <h1 className=' text-white '><small className='small'>Meet</small> <br/> Mayowa Tosin Adejumola</h1>
     </div>
-    <diV className='About-right'>
-    <p className='About-Rp'>
+    <diV className='col-xs-12 col-md-12 col-lg-8 p-4 mt-3 text-white'>
+    <p className='Abt-Rp'>
     I am a <span>Frontend Engineer with two years of experience. </span>I love Javascript, and I can tell you that is my strength,I love to make things work,solving complex problems.
     I have good knowledge of the following react stacks: Hooks,NEXTjs,React Router, Context Api,Redux,Material UI,Redux,React Query,Jest etc..
     My main stacks are: Javascript,React,Typescript,HTML5 and CSS3, Git.I am always ready to learn,open to corrections.
     </p>
-<p className='About-Rp pa'>
+<p className='mt-2 Abt-Rp '>
 I graduated at the top of my class with <span>Distinction</span>. As a good Mathematics student, I love solving complex problems, what many call complex problems, to me it is an oppotunity to know more.
-I am the guy that used is <span>house rent to buy Laptop,I had to stay at friends place for months</span> , enter into loan to pay for my courses,this is how much I love to code and to acquire Knowledge.
+<span  className='pa'>I am the guy that used is house rent to buy Laptop,I had to stay at friends place for months , enter into loan to pay for my courses,this is how much I love to code and to acquire Knowledge.</span>
 
 </p>
     
     </diV>
+    </div>
+    
     </div>
   )
 }
