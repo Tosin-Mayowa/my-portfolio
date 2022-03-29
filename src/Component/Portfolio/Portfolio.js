4087a72fe01c4a266bf7137efa435ddd77746043
Tosin Mayowa <toss800@gmail.com>
2022-03-29 16:50:24 +0100

    add working links

--- src/Component/Portfolio/Portfolio.js
+++ src/Component/Portfolio/Portfolio.js
@@ -13,19 +13,19 @@ function Portfolio(){
     <div className='col-xs-12 col-md-6 col-lg-4 p-1 mb-2 card'>
         <h3 className='card-title'>My Portfolio Website</h3>
         <p>I buit this project using React,Mui,React libraries</p>
-        <a href='https://eweb.com'><FaAddressBook size={20}/></a>
+        <a href='https://mayowa-portfolio.vercel.app/'><FaAddressBook size={20}/></a>
     </div>
 
     <div className='col-xs-12 col-md-6 col-lg-4 mb-2 p-1 card'>
         <h3 className='card-title'>Todo App</h3>
-        <p>I buit this project using Reacthooks and Material UI</p>
-        <a href='https://eweb.com'><FaAddressBook size={20}/></a>
+        <p>I buit this project using class component and CSS3</p>
+        <a href='https://mayowa-portfolio.vercel.app/'><FaAddressBook size={20}/></a>
     </div>
 
      <div className='col-xs-12 col-md-6 col-lg-4 p-1 mb-2 card'>
         <h3 className='card-title'>sign in form</h3>
         <p>This form has the ability to change from one language to anothe, you can also switch to dark mode, I used contextApi.</p>
-        <a href='https://eweb.com'><FaAddressBook size={20}/></a>
+        <a href='https://multilanguage-signin-form.vercel.app/'><FaAddressBook size={20}/></a>
     </div>
   </div>
     
