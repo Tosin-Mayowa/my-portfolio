851ab17b6c7c07b917a696d18ae335dd8672a12b
Tosin Mayowa <toss800@gmail.com>
2022-04-25 22:48:14 +0100

    add shopping cart link

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,9 @@
+import '@testing-library/jest-dom'
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('input should be initially empty', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const nameField = screen.getByRole("textbox",{name:/Name/});
+  expect(nameField.value).toBe('');
 });
--- src/Component/Portfolio/Portfolio.js
+++ src/Component/Portfolio/Portfolio.js
@@ -10,17 +10,19 @@ function Portfolio(){
    <div className='container-fluid'>
    <h2  className='Portfolio-H'>Project Cards</h2>
   <div className="row">
-    <div className='col-xs-12 col-md-6 col-lg-4 p-1 mb-2 card'>
+    <div className='col-xs-12 col-md-6 col-lg-4 mb-2 p-1 card'>
+        <h3 className='card-title'>Shopping Cart</h3>
+        <p>I built this cart using react-redux,redux core libraries,hooks and Bootstrap for design,all data used are in the application</p>
+        <a href='https://shopping-cart-psi-ten.vercel.app/'><FaAddressBook size={20}/></a>
+    </div>
+  
+  <div className='col-xs-12 col-md-6 col-lg-4 p-1 mb-2 card'>
         <h3 className='card-title'>My Portfolio Website</h3>
         <p>I buit this project using React,Mui,React libraries</p>
         <a href='https://mayowa-portfolio.vercel.app/'><FaAddressBook size={20}/></a>
     </div>
 
-    <div className='col-xs-12 col-md-6 col-lg-4 mb-2 p-1 card'>
-        <h3 className='card-title'>Todo App</h3>
-        <p>I buit this project using class component and CSS3</p>
-        <a href='https://mayowa-portfolio.vercel.app/'><FaAddressBook size={20}/></a>
-    </div>
+    
 
      <div className='col-xs-12 col-md-6 col-lg-4 p-1 mb-2 card'>
         <h3 className='card-title'>sign in form</h3>
