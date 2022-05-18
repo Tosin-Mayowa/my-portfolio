721a77b33386929ec1f478e4215ac406ab6b25a8
Tosin Mayowa <toss800@gmail.com>
2022-05-19 00:21:49 +0100

    Remove unused var

--- src/Component/Header/Header.js
+++ src/Component/Header/Header.js
@@ -1,5 +1,5 @@
 import React,{useState,useEffect} from 'react';
-import {Paper, Grid, Button, Avatar} from '@mui/material';
+import { Button, Avatar} from '@mui/material';
 import './Header.css';
 import Resume from '../Resume/Resume';
 import Mayowa from '../Image/Mayowa.jpg';
@@ -11,11 +11,7 @@ const Header = () => {
     const [isClicked, setIsclicked]=useState(false);
 
 
-/* const changeStatus=()=>{
-  setTimeout(() => {
-    setStatus(true);
-  }, 2000);
-} */
+
 
   useEffect(()=>{
     setTimeout(()=>{
@@ -56,39 +52,3 @@ const Header = () => {
 }
 
 export default Header;
-/* <Grid container >
-
-  <Grid item xs={12} md={12} lg={6}>
-
-    <Paper className='Paper-Header' style={{ height: '100vh', padding: '1rem 1rem' }} elevation={0}>
-      <div>
-        <h1 className='Typo-left'>I am a <br /> <span style={{ color: '#247ba0' }}>Frontend Engineer</span> with passion<br /> for solving complex problems</h1>
-      </div>
-
-      <div className='Avatar-div'>
-        <Avatar style={{
-          margin: '20px auto',
-
-          width: '150px',
-          height: '150px'
-        }} alt="Cindy Baker" src={Mayowa} />
-        <h6 className='Avatar-title' style={{ margin: '2px auto' }}> Mayowa Tosin Adejumola</h6>
-
-
-      </div>
-
-    </Paper>
-
-  </Grid>
-  <Grid item xs={12} md={12} lg={6}>
-    <Paper style={{ height: '100vh', paddingTop: '2rem' }} elevation={0}>
-      <div className='side-paper Paper' onMouseEnter={changeStatus} >
-        <h2 style={{ color: '#fff' }}>{status ? 'Thank you for checking' : 'Click to view my resume'}</h2>
-        <Button className='side-button' variant="outlined" onClick={() => { setIsclicked(!isClicked) }}>{isClicked ? 'Clicked' : 'click'}</Button>
-      </div>
-      {isClicked ? <Resume /> : <Skills />}
-    </Paper>
-
-  </Grid>
-
-</Grid> */
