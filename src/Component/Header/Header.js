4a38a146c31f81ff9f41b3273bc29a40ec5578ca
Tosin Mayowa <toss800@gmail.com>
2022-05-19 20:09:43 +0100

    Add images to portfolio

--- src/Component/Header/Header.js
+++ src/Component/Header/Header.js
@@ -23,7 +23,7 @@ const Header = () => {
        <div className="row">
           <div className="col-xs-12 col-md-6 col-lg-6 mt-3">
           <div>
-            <h1 className='Typo-left'> <br /> <span style={{ color: '#247ba0' }}>Frontend Engineer</span> with passion<br /> for solving complex problems</h1>
+            <h1 className='Typo-left'>I am <br /> <span style={{ color: '#247ba0' }}>Frontend Engineer</span> with passion<br /> for solving complex problems</h1>
           </div>
             <div className='Avatar-div'>
               <Avatar style={{
@@ -37,9 +37,9 @@ const Header = () => {
 
             </div>
           </div>
-          <div className="col-xs-12 col-md-6 col-lg-6mt-3">
+          <div className="col-xs-12 col-md-6 col-lg-6 mt-3">
           <div className='side-paper Paper' >
-            <h2 style={{ color: '#fff' }}>Click to view my resume</h2>
+            <h2 className='heading2' style={{ color: '#fff' }}>Click to view my resume</h2>
             <Button className='side-button' variant="outlined" onClick={() => { setIsclicked(!isClicked) }}>{isClicked ? 'Clicked' : 'click'}</Button>
           </div>
             {isClicked ? <Resume /> : <Skills />}
--- src/Component/Portfolio/Portfolio.js
+++ src/Component/Portfolio/Portfolio.js
@@ -3,8 +3,8 @@ import '../../../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import './Portfolio.css';
 import {FaAddressBook} from 'react-icons/fa';
 import Cart from '../Image/shopping-cart.jpg';
-
-
+import portf from '../Image/portf.jpg';
+import form from '../Image/form.jpg';
 function Portfolio(){
 
  return (
@@ -12,13 +12,14 @@ function Portfolio(){
    <h2  className='Portfolio-H'>Project Cards</h2>
   <div className="row">
     <div className='col-xs-12 col-md-6 col-lg-4 mb-2 p-1 card'>
-         <img src={Cart} class="card-img-top" alt="..." />
+         <img src={Cart} className="card-img-top" alt="..." />
         <h3 className='card-title'>Shopping Cart</h3>
         <p>I built this cart using react-redux,redux core libraries,hooks and Bootstrap for design,all data used are in the application</p>
         <a href='https://shopping-cart-psi-ten.vercel.app/'><FaAddressBook size={20}/></a>
     </div>
   
   <div className='col-xs-12 col-md-6 col-lg-4 p-1 mb-2 card'>
+         <img src={portf} className="card-img-top" alt="..." />
         <h3 className='card-title'>My Portfolio Website</h3>
         <p>I buit this project using React,Mui,React libraries</p>
         <a href='https://mayowa-portfolio.vercel.app/'><FaAddressBook size={20}/></a>
@@ -27,7 +28,8 @@ function Portfolio(){
     
 
      <div className='col-xs-12 col-md-6 col-lg-4 p-1 mb-2 card'>
-        <h3 className='card-title'>sign in form</h3>
+         <img src={form} className="card-img-top" alt="..." />
+     <h3 className='card-title'>sign in form</h3>
         <p>This form has the ability to change from one language to anothe, you can also switch to dark mode, I used contextApi.</p>
         <a href='https://multilanguage-signin-form.vercel.app/'><FaAddressBook size={20}/></a>
     </div>
