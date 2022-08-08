97aa40de5504677a8f7b26d0dfa7c21756c25f7f
Tosin Mayowa <toss800@gmail.com>
2022-08-08 14:09:42 +0100

    fix- portfolio

--- src/Component/Portfolio/Portfolio.js
+++ src/Component/Portfolio/Portfolio.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import '../../../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import './Portfolio.css';
-import {FaAddressBook} from 'react-icons/fa';
 import Cart from '../Image/shopping-cart.jpg';
 import portf from '../Image/portf.jpg';
 import form from '../Image/form.jpg';
@@ -15,14 +14,14 @@ function Portfolio(){
          <img src={Cart} className="card-img-top" alt="..." />
         <h3 className='card-title'>Shopping Cart</h3>
         <p>I built this cart using react-redux,redux core libraries,hooks and Bootstrap for design,all data used are in the application</p>
-        <a href='https://shopping-cart-psi-ten.vercel.app/'><FaAddressBook size={20}/></a>
+         <a href='https://shopping-cart-psi-ten.vercel.app/' class="text-decoration-none">View</a>
     </div>
   
   <div className='col-xs-12 col-md-6 col-lg-4 p-1 mb-2 card'>
          <img src={portf} className="card-img-top" alt="..." />
         <h3 className='card-title'>My Portfolio Website</h3>
         <p>I buit this project using React,Mui,React libraries</p>
-        <a href='https://mayowa-portfolio.vercel.app/'><FaAddressBook size={20}/></a>
+         <a href='https://mayowa-portfolio.vercel.app/' class="text-decoration-none">View</a>
     </div>
 
     
@@ -31,7 +30,7 @@ function Portfolio(){
          <img src={form} className="card-img-top" alt="..." />
      <h3 className='card-title'>sign in form</h3>
         <p>This form has the ability to change from one language to anothe, you can also switch to dark mode, I used contextApi.</p>
-        <a href='https://multilanguage-signin-form.vercel.app/'><FaAddressBook size={20}/></a>
+         <a href='https://multilanguage-signin-form.vercel.app/' class="text-decoration-none">View</a>
     </div>
   </div>
     
